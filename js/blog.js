c21a13592c6050584cad7765dcdbc16c2176ef7c
--add <alex@bluebandmedia.com>
2015-01-27 21:49:06 -0500

    ....

--- js/blog.js
+++ js/blog.js
@@ -13,15 +13,6 @@ $(function() {
 
     var blogs = new Blogs();
 
-    blogs.fetch({
-        success: function(blogs) {
-            console.log(blogs);
-        },
-        error: function(blogs, error) {
-            console.log(error);
-        }
-    });
-
     var BlogsView = Parse.View.extend({
         template: Handlebars.compile($('#blogs-tpl').html()),
         render: function() {
@@ -29,6 +20,19 @@ $(function() {
                 blog: this.collection.toJSON()
             };
             this.$el.html(this.template(collection));
+        }
+    });
+
+    blogs.fetch({
+        success: function(blogs) {
+            var blogsView = new BlogsView({
+                collection: blogs
+            });
+            blogsView.render();
+            $('.main-container').html(blogsView.el);
+        },
+        error: function(blogs, error) {
+            console.log(error);
         }
     });
 
